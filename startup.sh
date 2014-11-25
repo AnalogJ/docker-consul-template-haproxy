e03b3bf49734ef5eddce9fa5cfdec163afe8df01
Jason Kulatunga <kulatung@adobe.com>
2014-11-24 17:22:14 -0800

    removed debugging cmds

--- startup.sh
+++ startup.sh
@@ -8,8 +8,5 @@ cd "$HAPROXY"
 
 haproxy -f "$CONFIG_FILE" -p "$PIDFILE" -D -st $(cat $PIDFILE)
 
-uname -m
-uname -a
-
 /usr/local/bin/consul-template -consul=${CONSUL_PORT_8500_TCP_ADDR}:${CONSUL_PORT_8500_TCP_PORT} \
   -template "/etc/haproxy/haproxy.template:/etc/haproxy/haproxy.cfg:/hap.sh"
